9c6ba69ed488fbcc6731f28300bba51ef97eace3
Nicolás Valdés <github@nicogit.dev>
2026-08-06 17:50:13 -0400

    fix(display): adapt inverse display toggle
    Modified the ST7565 driver menu logic to use bitwise XOR instead of OR. This preserves the hardcoded 0xA7 default inverted display I've implemented while allowing the menu toggle to correctly revert to normal display when enabled.

--- driver/st7565.c
+++ driver/st7565.c
@@ -182,7 +182,7 @@ uint8_t cmds[] = {
     ST7565_CMD_BIAS_SELECT | 0,             // Select bias setting: 1/9
     ST7565_CMD_COM_DIRECTION  | (0 << 3),   // Set output direction of COM: normal
     ST7565_CMD_SEG_DIRECTION | 1,           // Set scan direction of SEG: reverse
-    ST7565_CMD_INVERSE_DISPLAY | 0,         // Inverse Display: false
+    ST7565_CMD_INVERSE_DISPLAY ^ 0,         // Inverse Display: true (quirk default)
     ST7565_CMD_ALL_PIXEL_ON | 0,            // All Pixel ON: false - normal display
     ST7565_CMD_REGULATION_RATIO | (4 << 0), // Regulation Ratio 5.0
 
@@ -199,7 +199,7 @@ uint8_t cmds[] = {
     {
         switch(i) {
             case 3:
-                ST7565_WriteByte(ST7565_CMD_INVERSE_DISPLAY | gSetting_set_inv);
+                ST7565_WriteByte(ST7565_CMD_INVERSE_DISPLAY ^ gSetting_set_inv);
                 break;
             case 7:
                 ST7565_WriteByte(21 + gSetting_set_ctr);
